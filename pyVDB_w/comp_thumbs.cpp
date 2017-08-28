8e30ef47e7f95cc7688a37bc617237420641ec05
pagebrian3 <pagebrian3@gmail.com>
2017-08-28 16:19:30 -0400

    Fix reading issue.

--- pyVDB_w/comp_thumbs.cpp
+++ pyVDB_w/comp_thumbs.cpp
@@ -14,19 +14,6 @@
 #define CUT_THRESH 1000.0
 #define EXTENSION ".png"
 #define HOME_PATH "/home/ungermax/.video_proj/"
-#define ThrowWandException(wand)					\
-  {									\
-    char								\
-      *description;							\
-									\
-    ExceptionType							\
-      severity;								\
-									\
-    description=MagickGetException(wand,&severity);			\
-    (void) fprintf(stderr,"%s %s %ld %s\n",GetMagickModule(),description); \
-    description=(char *) MagickRelinquishMemory(description);		\
-    exit(-1);								\
-  }
 
 int main(int argc, char* argv[])
 {
@@ -46,14 +33,15 @@ int main(int argc, char* argv[])
   double start_time = TRACE_TIME;
   if(length <= TRACE_TIME) start_time=0.0; 
   double frame_spacing = (length-start_time)/BORDER_FRAMES;
-  std::cout <<(boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %.03d -vframes 1 %s") % path % start_time % imgp ).str().c_str() << std::endl;
+  //std::cout <<(boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %.03d -vframes 1 %s") % path % start_time % imgp ).str().c_str() << std::endl;
   std::system((boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %.03d -vframes 1 %s") % path % start_time % imgp ).str().c_str());
   std::vector<boost::filesystem::directory_entry> bmpList;
   MagickWandGenesis();
   MagickWand *image_wand1=NewMagickWand();
   MagickWand *image_wand2=NewMagickWand();
-  MagickBooleanType status=MagickReadImage(image_wand2,imgp);
-  if (status == MagickFalse) ThrowWandException(image_wand1);
+  PixelIterator* iterator;
+  PixelWand ** pixels;
+  MagickReadImage(image_wand2,imgp);
   MagickPixelPacket pixel;
   std::vector<double> blah;
   unsigned long width,height;
@@ -61,8 +49,7 @@ int main(int argc, char* argv[])
   long y;
   height = MagickGetImageHeight(image_wand2);
   width = MagickGetImageWidth(image_wand2);
-  if (status == MagickFalse) ThrowWandException(image_wand2);
-  std::cout << "BLAH " <<status<<" "<< width <<" "<<height<<std::endl;
+  //std::cout << "BLAH " << width <<" "<<height<<std::endl;
   std::vector<double> rowSums(height);
   std::vector<double> colSums(width);
   double corrFactorCol = 1.0/(double)(BORDER_FRAMES*height);
@@ -73,20 +60,22 @@ int main(int argc, char* argv[])
     std::system((boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %d -vframes 1 %s") % path % start_time % imgp ).str().c_str());
     MagickReadImage(image_wand2,imgp);
     MagickCompositeImage(image_wand1,image_wand2,DifferenceCompositeOp, 0,0);
-    PixelIterator* iterator = NewPixelIterator(image_wand1);
+    iterator = NewPixelIterator(image_wand1);
     for (y=0; y < height; y++)
       {
 	//std::cout << x <<" "<<y << std::endl;
-	PixelWand ** pixels=PixelGetNextIteratorRow(iterator,&width);
+	pixels=PixelGetNextIteratorRow(iterator,&width);
 	if (pixels == (PixelWand **) NULL) break;
 	for (x=0; x < (long) width; x++)
 	  {
 	    PixelGetMagickColor(pixels[x],&pixel);
-	    double value =sqrt(1.0/3.0*(pow(pixel.red,2.0)+pow(pixel.green,2.0)+pow(pixel.blue,2.0)));	    
+	    double value =sqrt(1.0/3.0*(pow(pixel.red,2.0)+pow(pixel.green,2.0)+pow(pixel.blue,2.0)));
 	    rowSums[y]+=corrFactorRow*value;
 	    colSums[x]+=corrFactorCol*value;
 	  }
+	//PixelSyncIterator(iterator);
       }
+    //iterator=DestroyPixelIterator(iterator);
     if(rowSums[0] > CUT_THRESH &&
        rowSums[height-1] > CUT_THRESH &&
        colSums[0] > CUT_THRESH &&
@@ -99,9 +88,9 @@ int main(int argc, char* argv[])
   while(rowSums[y2] < CUT_THRESH) y2--;
   x2-=x1-1;  //the -1 corrects for the fact that x2 and y2 are sizes, not positions
   y2-=y1-1;
-  std::cout << x2 << " "<<y2 <<" "<<x1 <<" "<<y1 << std::endl;
+  //std::cout << x2 << " "<<y2 <<" "<<x1 <<" "<<y1 << std::endl;
   boost::filesystem::remove(imgp);
-  std::cout << (boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %d -vf fps=%d -filter \"crop=%i:%i:%i:%i,scale=2x2\" %s/out%%05d%s") % path % TRACE_TIME % TRACE_FPS % x2 % y2 % x1 % y1 %home % EXTENSION).str().c_str();
+  //std::cout << (boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %d -vf fps=%d -filter \"crop=%i:%i:%i:%i,scale=2x2\" %s/out%%05d%s") % path % TRACE_TIME % TRACE_FPS % x2 % y2 % x1 % y1 %home % EXTENSION).str().c_str();
   std::system((boost::format("ffmpeg -y -nostats -loglevel 0 -i \"%s\" -ss %d -vf fps=%d -filter \"crop=%i:%i:%i:%i,scale=2x2\" %s/out%%05d%s") % path % TRACE_TIME % TRACE_FPS % x2 % y2 % x1 % y1 %home % EXTENSION).str().c_str());
   bmpList.clear();
   boost::filesystem::path p(home);
@@ -111,22 +100,25 @@ int main(int argc, char* argv[])
   std::vector<int> data(12*listSize);
   int i = 0;
   double scale = 1.0/256.0;
+  ClearMagickWand(image_wand1);
   for(auto tFile: bmpList) {
-    MagickReadImage(image_wand1,tFile.path().c_str());
-    PixelIterator *iterator = NewPixelIterator(image_wand1);
+    auto status = MagickReadImage(image_wand1,tFile.path().c_str());
+    iterator = NewPixelIterator(image_wand1);
     for (y=0; y < 2; y++)
       {
-	PixelWand ** pixels=PixelGetNextIteratorRow(iterator,&width);
+	pixels=PixelGetNextIteratorRow(iterator,&width);
 	if (pixels == (PixelWand **) NULL) 
 	  break;
 	for (x=0; x < 2; x++)
 	  {
-	    PixelGetMagickColor(pixels[x],&pixel);
+	    PixelGetMagickColor(pixels[x],&pixel);    
 	    data[12*i+6*y+3*x]=scale*pixel.red;
 	    data[12*i+6*y+3*x+1]=scale*pixel.green;
 	    data[12*i+6*y+3*x+2]=scale*pixel.blue;
-	  }   
+	  }
+	//PixelSyncIterator(iterator);
       }
+    //iterator=DestroyPixelIterator(iterator);
     i++;
   }
   char outfile[100]="trace";
@@ -137,7 +129,7 @@ int main(int argc, char* argv[])
   for(auto trace_dat: data) *outFile << trace_dat <<"\t";
   outFile->close();
   delete outFile;
-  //boost::filesystem::remove(temp_dir);
+  boost::filesystem::remove_all(temp_dir);
   return 0;
 }
 
