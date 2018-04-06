4733a716db39304f6dcfc4291ba7984a0b6fd098
Brian Page <pagebrian3@gmail.com>
2018-04-06 11:05:41 -0400

    Fix issue with P6_NOP config and implement CPU vendor auto-selection.

--- custom_kbuild/prefix_config.sh
+++ custom_kbuild/prefix_config.sh
@@ -4,3 +4,18 @@ for i in $APPEND_LIST
 do
     sed -i "\$aCONFIG_${i}" .config
 done
+
+COMMENT_LIST="GENERIC_CPU"
+
+for i in $COMMENT_LIST
+do
+    sed -i "/^CONFIG_${i}=/c\# CONFIG_${i} is not set" .config
+done
+
+YES_LIST="MNATIVE"
+
+for i in $YES_LIST
+do
+    sed -i "/# CONFIG_${i} is not set/c\CONFIG_${i}=y" .config
+done
+
