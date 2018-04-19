7c214c905b11a509249f83b81620eaebf6090838
Brian Page <pagebrian3@gmail.com>
2018-04-19 16:29:04 -0400

    Stuff.

--- VDBMT/VideoIcon.h
+++ VDBMT/VideoIcon.h
@@ -1,22 +1,19 @@
 #ifndef VIDEOICON_H
 #define VIDEOICON_H
 #include "VidFile.h"
-#include <gtkmm-3.0/gtkmm.h>
+#include <gtkmm-3.0/gtkmm/image.h>
 
 class VideoIcon : public Gtk::Image
 {
   public:
 
   VideoIcon(VidFile * vidFile);
-
   virtual ~VideoIcon();
-
   VidFile * get_vid_file();
 
  protected:
 
   VidFile* fVidFile;
-  
 };
 
 #endif  //  VIDEOICON_H
