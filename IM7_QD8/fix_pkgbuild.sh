19698294be37f02757a4bdf83c79f648d4960aa4
ungermax <pagebrian3@gmail.com>
2019-03-28 19:02:18 -0400

    EVEN MORE!

--- IM7_QD8/fix_pkgbuild.sh
+++ IM7_QD8/fix_pkgbuild.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sed -i 's/libmagick\ imagemagick/libmagick-qd8\ imagemagick/' PKGBUILD
 sed -i 's/libmagick()/libmagick-qd8()/g' PKGBUILD
 sed -i '/\.\/configure/a \ \ \  --disable-docs \\' PKGBUILD
 sed -i '/\.\/configure/a \ \ \  --with-quantum-depth=8 \\' PKGBUILD
@@ -15,11 +14,13 @@ for config2 in $cfg_list2
 do
     sed -i "s/--with-$config2/--without-$config2/" PKGBUILD
 done
+sed -i 's/^pkgname=.*/pkgname=libmagick-qd8/' PKGBUILD
 sed -i '/perl-options/d' PKGBUILD
 sed -i '/install -Dt/d' PKGBUILD
 sed -i '/make\ check/d' PKGBUILD
 sed -i '/\"$pkgdir\/usr\/lib\/perl5\"\ usr\/lib\//d' PKGBUILD
 sed -i '/\"$pkgdir\/usr\/share\/man\"\ usr\/share\//d' PKGBUILD
+sed -i 's/mv\ \"\$pkgdir\/usr\/bin\/\"\ usr\//rm\ -r\ \"$pkgdir\/usr\/bin\/\"\ /' PKGBUILD
 sed -i '/find \"$pkgdir\/usr\/lib\/perl5\"\ -name/d' PKGBUILD
 sed -i '/^makedepends/ {s/openexr//;s/libwmf//;s/librsvg//;s/libwebp//;s/libraw//;}' PKGBUILD
 sed -i '/chrpath ocl-icd/ {s/libheif//;s/jbigkit//;}' PKGBUILD
@@ -29,4 +30,4 @@ sed -i '/srcdir\/docpkg\/usr\/share\//a \ \ rm\ -r\ \"$pkgdir\"\/usr\/share' PKG
 sed -i '/srcdir\/docpkg\/usr\/share\//a \ \ find\ \"$pkgdir\"\/usr\/lib\/pkgconfig\/\ -type\ f\ !\ -name\ \"\*Q8\.pc\"\ -delete' PKGBUILD
 sed -i '/srcdir\/docpkg\/usr\/share\//a \ \ sed\ -i\ \x27\/\^Requires:\/\ s\/$\/-7\\\.Q8\/\x27\ $pkgdir\/usr\/lib\/pkgconfig\/\*\.pc' PKGBUILD
 sed -i '/pkgdir\/usr\/share\/doc/d' PKGBUILD
-
+sed -i '/package\ imagemagick()/,$d' PKGBUILD
