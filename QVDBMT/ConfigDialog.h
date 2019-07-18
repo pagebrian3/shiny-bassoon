b4426c19be4b159ff2c982e853cfd68ae98b781a
ungermax <pagebrian3@gmail.com>
2019-07-18 12:11:32 -0400

    Make all dialogs header-only.

--- QVDBMT/ConfigDialog.h
+++ QVDBMT/ConfigDialog.h
@@ -1,10 +1,13 @@
+#include "QVBConfig.h"
 #include <QDialog>
-#include <boost/variant.hpp>  //can't fwd declare  its a templated type
-
-class QWidget;
-class QFormLayout;
-class QLineEdit;
-class qvdb_config;
+#include <QWidget>
+#include <QDialogButtonBox>
+#include <QFormLayout>
+#include <QVBoxLayout>
+#include <QGroupBox>
+#include <QLineEdit>
+#include <QScrollArea>
+#include <boost/variant.hpp>
 
 class ConfigDialog: public QDialog {
 
@@ -12,14 +15,66 @@ class ConfigDialog: public QDialog {
 
 public:
 
-  ConfigDialog(QWidget * parent, qvdb_config * cfg);
+ConfigDialog(QWidget * parent, qvdb_config * cfg)  {
+  cfgPtr = cfg;
+  config = cfg->get_data();
+  setMaximumHeight(cfgPtr->get_int("win_height"));
+  QGroupBox * flob = new QGroupBox;
+  QScrollArea * scrollArea = new QScrollArea;
+  scrollArea->setWidget(flob);
+  formLayout = new QFormLayout;
+  for(auto & a: config) {
+    int type = a.second.which();
+    QLineEdit *  tempEdit = new QLineEdit();
+    std::string tempS;
+    if(type == 0) tempS = std::to_string(boost::get<int>(a.second));
+    else if(type == 1) tempS = std::to_string(boost::get<float>(a.second));
+    else tempS = boost::get<std::string>(a.second);
+    tempEdit->setPlaceholderText(tempS.c_str());
+    editPtrs.push_back(tempEdit);
+    formLayout->addRow(a.first.c_str(),tempEdit);
+  }
+  flob->setLayout(formLayout);
+  QDialogButtonBox * buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+  connect(buttonBox,&QDialogButtonBox::accepted,this,&ConfigDialog::on_accept);
+  connect(buttonBox,&QDialogButtonBox::accepted,this,&QDialog::accept);
+  connect(buttonBox,&QDialogButtonBox::rejected,this,&QDialog::reject);
+  QVBoxLayout *mainLayout = new QVBoxLayout;
+  mainLayout->addWidget(flob);
+  mainLayout->addWidget(buttonBox);
+  setLayout(mainLayout);
+  setWindowTitle(tr("Video Browser Configuration")); 
+};
 
-  void on_accept();
+void on_accept() {
+  int i = 0;
+  for(auto & cfg:config) {
+    std::string key(cfg.first);
+    QLineEdit * leHolder = editPtrs[i];
+    i++;
+    if(leHolder->isModified()) {
+      int index = cfg.second.which();
+      if(index == 0) {
+	cfgPtr->set(key,leHolder->text().toInt());
+      }
+      else if(index == 1) {
+	cfgPtr->set(key,leHolder->text().toFloat());
+      }
+      else if(index == 2) {
+	cfgPtr->set(key,leHolder->text().toStdString());
+      }
+      else continue;
+    }
+    else continue;
+  }
+  return;
+};
 
-private:
+  private:
 
   qvdb_config * cfgPtr;
   QFormLayout * formLayout;
   std::vector<QLineEdit *> editPtrs;
   std::map<std::string,boost::variant<int, float, std::string> > config;
 };
+
